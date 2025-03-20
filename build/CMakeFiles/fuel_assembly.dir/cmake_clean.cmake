3455dd7bdf4e5f27b4bbb308738d756529c90956
bgreen-01 <195755855+bgreen-01@users.noreply.github.com>
2025-03-20 11:45:36 +0000

    Delete build directory

--- build/CMakeFiles/fuel_assembly.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/fuel_assembly"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/fuel_assembly.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/CMakeFiles/fuel_assembly.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for fuel_assembly.
-# This may be replaced when dependencies are built.
